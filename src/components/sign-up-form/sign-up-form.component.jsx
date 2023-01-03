a3e8a4ebd5df45b9d6e9c8a1c1c1faa3aceae142
Aston Victor <vaston56@gmail.com>
2023-01-03 09:12:30 +0100

    created our authentication page alongside our sign in form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -89,7 +89,7 @@ const SignUpForm = () => {
                     name='confirmPassword'
                     value={confirmPassword}
                />
-               <Button type="submit"> Sign Up </Button>
+               <Button type="submit"> SIGN UP </Button>
             </form>
         </div>
     )
