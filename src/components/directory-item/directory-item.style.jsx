476c83519b5dc68d2f6b6cb6b035e63d5d6ddfb9
Vicaston <macbookpro@MACBOOKs-MacBook-Pro.local>
2023-04-24 13:53:08 +0100

    Made the web mobile responsive

--- src/components/directory-item/directory-item.style.jsx
+++ src/components/directory-item/directory-item.style.jsx
@@ -55,6 +55,10 @@ export const DirectoryItemContainer = styled.div`
     & ${Body} {
       opacity: 0.9;
     }
+
+    @media screen and (max-width: 800px) {
+      height: 200px;
+    }
   }
 
   &:first-child {
